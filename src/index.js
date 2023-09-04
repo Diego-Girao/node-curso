68e36d6c7a20d2ff61f35c67e4d0a460ac2a3c8f
diegogirao <diegoramiro25@gmail.com>
2023-09-04 20:41:27 +0100

    feat: created api methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,62 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response) => {
-	return response.json({ message: "Hey World of Nodejs!!!" })
+app.use(express.json())
+
+app.get("/teams", (request, response) => {
+	const query = request.query
+	console.log(query)
+	return response.json([
+		"Benfica",
+		"FC Porto",
+		"Sporting",
+		"SC Braga",
+		"FC Famalicão",
+	])
+})
+
+app.post("/teams", (request, response) => {
+	const body = request.body
+	console.log(body)
+	return response.json([
+		"Benfica",
+		"FC Porto",
+		"Sporting",
+		"SC Braga",
+		"FC Famalicão",
+		"Est. Amadora",
+	])
+})
+
+app.put("/teams/:id", (request, response) => {
+	return response.json([
+		"Benfica",
+		"FC Porto",
+		"Sporting Lisboa",
+		"SC Braga",
+		"FC Famalicão",
+		"Est. Amadora",
+	])
+})
+app.patch("/teams/:id", (request, response) => {
+	return response.json([
+		"Benfica",
+		"FC Porto",
+		"Sporting Lisboa",
+		"SC Braga",
+		"FC Famalicão",
+		"Estrela Amadora",
+	])
+})
+
+app.delete("/teams/:id", (request, response) => {
+	return response.json([
+		"Benfica",
+		"FC Porto",
+		"Sporting Lisboa",
+		"SC Braga",
+		"Est. Amadora",
+	])
 })
 
 app.listen(3000)
